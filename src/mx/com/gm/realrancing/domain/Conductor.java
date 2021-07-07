846c57f87aa5f6f3e30b41d6040e28e2f8a08590
Richard Vellojin <richardvellojin@gmail.com>
2021-07-07 14:18:27 -0500

    Modificando Clase conductor

--- src/mx/com/gm/realrancing/domain/Conductor.java
+++ src/mx/com/gm/realrancing/domain/Conductor.java
@@ -8,33 +8,46 @@ import java.util.*;
  */
 public class Conductor {
     
-    private String nombre;
-    
+   
+    List<String>nombres = new ArrayList<String>();
+
     public Conductor(){
         
     }
-    public Conductor(String nombre) {
-        this.nombre = nombre;
-    }
 
-    public String getNombre() {
-        return this.nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    public void agregarJugador(){
+        Scanner consola = new Scanner(System.in);
+        System.out.println("Cuantos Jufadores son");
+        int cantidad =Integer.parseInt(consola.nextLine());
+        int contador=0;
+        while(contador<cantidad){
+            System.out.println("Ingrese el nombre de los jugadores");
+            String nombre=consola.nextLine();
+            nombres.add(nombre);
+            contador+=1;
+        }
+        
+        }
     
-
+    public void listarJugadores(){
+        for (String nombre : nombres) {
+            System.out.println("Lista de jugadores");
+            System.out.println(nombre);
+            
+        }
+    }
     @Override
-    public String toString() { 
-        return nombre;
+    public String toString() {
+        return "Conductor{" + "nombres=" + nombres + '}';
     }
-
-    
-    
-    
-    
-    
+   
     
 }
+        
+        
+    
+    
+    
+    
+    
+
