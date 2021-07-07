f5c1b8f2cce7158f412d71c15fd8c11ebaf06405
Richard Vellojin <richardvellojin@gmail.com>
2021-07-07 17:50:22 -0500

    Modificando Clase pista

--- src/mx/com/gm/realrancing/domain/Pista.java
+++ src/mx/com/gm/realrancing/domain/Pista.java
@@ -1,24 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mx.com.gm.realrancing.domain;
 
 import java.util.*;
+
 
 /**
  *
  * @author Richard Vellojin
  */
-public class Pista extends Conductor{
+public class Pista extends Conductor {
 
+    //Atirubutos clase Pista
     private int tamañoPista;
-   
-    
+    private int carriles;
+    private int valorDado;
     public Pista() {
     }
 
+    //metodos Get and Set
     public Pista(int tamañoPista) {
         this.tamañoPista = tamañoPista;
     }
@@ -31,19 +29,47 @@ public class Pista extends Conductor{
         this.tamañoPista = tamañoPista;
     }
     
-   
-    public void comenzarCarrera(){
-        int contador=0;
-        
-        while(contador<nombres.size()){
-            for (int i=0; i<nombres.size();i++) {
-               
-                
-            }
-            
-            contador+=1;
-        }
-        
+    public int getCarriles() {
+        return carriles;
+    }
+
+    public void setCarriles(int carriles) {
+        this.carriles = carriles;
+    }    
+    
+    //Metodos solucion 
+    
+    public int getValorDado() {
+        return this.valorDado;
+    }
+
+    public void setValorDado(int valorDado) {
+        this.valorDado = valorDado;
+    }
+
+    public void dados() {
+        Scanner consola=new Scanner(System.in); 
+        int opcion=0;
+        System.out.println("Jugador 1 para lanza el dado marque 1");
+                        opcion = Integer.parseInt(consola.nextLine());
+                        if (opcion == 1) {
+                        this.valorDado = (int)(Math.random() * 6 + 1);
+                           
+    }
     }
     
+    public void carriles(){
+       this.carriles=0;
+       int contador=0;
+       while(contador<nombres.size()){
+           carriles+=1;
+           contador+=1;
+       }
+        System.out.println("Cantidad de carriles: "+carriles);
+    }
+    
+          
+
+    
+
 }
