977807dfd633f3d5030414cdaa033ea9bd4548b4
Richard Vellojin <richardvellojin@gmail.com>
2021-07-08 10:35:48 -0500

    Creando sub clases para excepciones

---
+++ src/mx/com/gm/realrancing/excepciones/EscriturDatosEx.java
@@ -0,0 +1,17 @@
+package mx.com.gm.realrancing.excepciones;
+
+/**
+ *
+ * @author Richard Vellojin
+ */
+
+public class EscriturDatosEx extends AccesoDatosEx{
+    
+    public EscriturDatosEx(String mensaje) {
+        super(mensaje);
+    }
+    
+
+        
+    
+}
