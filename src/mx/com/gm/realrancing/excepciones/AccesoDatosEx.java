b70c93ad3b05be6b556ff3921cf1b3d54a73b01b
Richard Vellojin <richardvellojin@gmail.com>
2021-07-08 10:34:49 -0500

    Creando clases para excepciones

---
+++ src/mx/com/gm/realrancing/excepciones/AccesoDatosEx.java
@@ -0,0 +1,17 @@
+package mx.com.gm.realrancing.excepciones;
+
+
+/**
+ *
+ * @author Richard Vellojin
+ */
+
+public class AccesoDatosEx extends Exception{
+    
+    public AccesoDatosEx(String mensaje){
+        super(mensaje);
+    }
+    
+    
+  
+}
